cc0cc8d623e52398213bcf02a8001a2808d1f5fd
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2025-03-18 22:39:57 +0800

    Delete HW1/myhello directory

--- HW1/myhello/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class MyhelloConfig(AppConfig):
-    name = 'myhello'
--- HW1/myhello/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class MyhelloTests(TestCase):
-    def test_example(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
